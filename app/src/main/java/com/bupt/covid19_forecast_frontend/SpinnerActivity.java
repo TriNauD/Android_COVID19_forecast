59fd13a02f268101c71b00b4915a6ac95ab8e4db
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-12 20:33:59 +0800

    去掉了刚刚添加的无用代码，添加日志调试

--- app/src/main/java/com/bupt/covid19_forecast_frontend/SpinnerActivity.java
+++ app/src/main/java/com/bupt/covid19_forecast_frontend/SpinnerActivity.java
@@ -2,6 +2,7 @@ package com.bupt.covid19_forecast_frontend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -17,9 +18,11 @@ public class SpinnerActivity extends AppCompatActivity implements AdapterView.On
 
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
+        //日志调试
+        final String TAG = "onItemSelected";
+        Log.i(TAG, "onItemSelected 进入函数");
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
-        parent.getItemAtPosition(pos);
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
