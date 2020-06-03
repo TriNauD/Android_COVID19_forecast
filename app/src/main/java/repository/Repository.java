8e746d7503df3e99ae9f3f9a8ef1a8fca28f2a91
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-06-03 19:13:26 +0800

    分离数据初始化（随机数和二次函数）

--- app/src/main/java/repository/Repository.java
+++ app/src/main/java/repository/Repository.java
@@ -1,4 +1,70 @@
 package repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Repository {
+    //真实线数
+    public static final int numOfRealLines = 4;
+    //预测线数
+    public static final int numOfForecastLines = 1;
+    //“真实线”的节点数
+    public static final int numOfRealPoints = 120;
+    //“预测线”的节点数
+    public static final int numOfForecastPoints = 15;
+
+    //预测参数
+    //是否进行控制
+    public boolean hasControl = false;
+    //控制开始时间
+    public String startControlDate;
+    //控制增长阶段的时间
+    public int raiseLastTime;
+    //控制强度
+    public int controlGrade;
+
+    //所有线数据
+    public List<float[]> lineData = new ArrayList<>();
+
+    //网络传进来的数
+    float[] x;
+    float[] y;
+    String[] date;
+
+    public Repository() {
+        x = new float[]{0, 1, 2, 3};
+        y = new float[]{0, 100, 200, 300};
+        date = new String[]{"1/1", "1/2", "1/3", "1/4"};
+        initReal();
+        initForecast();
+    }
+
+    public void initReal() {
+        for (int i = 0; i < numOfRealLines; ++i) {
+            float[] linePoints = new float[numOfRealPoints];//一条线上面的点
+            for (int j = 0; j < numOfRealPoints; ++j) {
+                Random random = new Random();
+                linePoints[j] = random.nextInt(50) + j * 10;
+            }
+            lineData.add(linePoints);
+        }
+    }
+
+    public void initForecast(){
+        for (int i = 0; i < numOfForecastLines; ++i) {
+            float[] linePoints = new float[numOfForecastPoints];//一条线上面的点
+            for (int j = 0; j < numOfForecastPoints; ++j) {
+                if (hasControl) {
+                    //如果进行控制
+                    linePoints[j] = 1200 - j * j;
+                } else {
+                    //群体免疫
+                    float x = j * 1000;
+                    linePoints[j] = 1200 + (float) Math.sqrt(x);
+                }
+            }
+            lineData.add(linePoints);
+        }
+    }
 }
--- app/src/main/java/viewModel/LineViewModel.java
+++ app/src/main/java/viewModel/LineViewModel.java
@@ -6,41 +6,28 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.PointValue;
 
+import repository.Repository;
+
 public class LineViewModel extends ViewModel {
     //调试使用的日志标签
     private static final String TAG = "LineViewModel";
 
-    //“真实线”的数量
-    private int numOfRealLines = 4;
-    //“真实线”的节点数
-    private int numOfRealPoints = 120;
-
     //按照先“真实”后“预测”的顺序：
     //所有线数据
-    private List<float[]> lineData = new ArrayList<>();
+    private Repository repository = new Repository();
+    private List<float[]> lineData = repository.lineData;
     //所有线
     private List<Line> lines = new ArrayList<>();
     //所有坐标轴
     private List<Axis[]> axesList = new ArrayList<>();
     //所有坐标轴的标签信息
     private List<List<String>> axisLableList = new ArrayList<>();
-
-    //预测参数
-    //是否进行控制
-    public boolean hasControl = false;
-    //控制开始时间
-    public String startControlDate;
-    //控制增长阶段的时间
-    public int raiseLastTime;
-    //控制强度
-    public int controlGrade;
 
     /**
      * 初始化预测的图表
@@ -52,34 +39,13 @@ public class LineViewModel extends ViewModel {
     public void initForecastChart() {
         Log.i(TAG, "initForecastChart 进入函数");
         //————预测————
-        //数
-        //“预测线”的数量
-        int numOfForecastLines = 1;
-        //“预测线”的节点数
-        int numOfForecastPoints = 15;
-        for (int i = 0; i < numOfForecastLines; ++i) {
-            float[] linePoints = new float[numOfForecastPoints];//一条线上面的点
-            for (int j = 0; j < numOfForecastPoints; ++j) {
-                if (hasControl) {
-                    //如果进行控制
-                    Log.i(TAG, "initForecastChart 函数：进行控制");
-                    linePoints[j] = 1200 - j * j;
-                } else {
-                    //群体免疫
-                    Log.i(TAG, "initForecastChart 函数：群体免疫");
-                    float x = j * 1000;
-                    linePoints[j] = 1200 + (float) Math.sqrt(x);
-                }
-            }
-            lineData.add(linePoints);
-        }
         //线
-        for (int i = 0; i < numOfForecastLines; i++) {
+        for (int i = 0; i < Repository.numOfForecastLines; i++) {
             //绑定数据
             List<PointValue> tempArrayList = new ArrayList<>();//一条线的数据
-            for (int p = 0; p < numOfForecastPoints; p++) {
-                int x = p + numOfRealPoints;//预测点接在真实后面
-                float y = lineData.get(i + numOfRealLines)[p];
+            for (int p = 0; p < Repository.numOfForecastPoints; p++) {
+                int x = p + Repository.numOfRealPoints;//预测点接在真实后面
+                float y = lineData.get(i + Repository.numOfRealLines)[p];
                 tempArrayList.add(new PointValue(x, y));//在真实线后面的是预测线
             }
             //设置样式
@@ -96,11 +62,11 @@ public class LineViewModel extends ViewModel {
         //轴
         //todo 预测的坐标轴应该是真实轴的延申
         //建立预测标签
-        for (int i = 0; i < numOfForecastLines; i++) {
+        for (int i = 0; i < Repository.numOfForecastLines; i++) {
             //对于每一条预测线
             List<String> strings = new ArrayList<>();
             int day = 0;
-            for (int j = 0; j < numOfForecastPoints; j++) {
+            for (int j = 0; j < Repository.numOfForecastPoints; j++) {
                 day++;
                 if (day > 30) {
                     day %= 30;
@@ -112,23 +78,23 @@ public class LineViewModel extends ViewModel {
             axisLableList.add(strings);
         }
         //绑定标签和轴
-        for (int i = 0; i < numOfForecastLines; i++) {
+        for (int i = 0; i < Repository.numOfForecastLines; i++) {
             //每条线
             Axis axisX = new Axis();//新建一个x轴
             List<AxisValue> valueListX = new ArrayList<>();//新建一个x轴的值列表
             //每个点，包括前面真实和后面预测
-            for (int p = 0; p < numOfRealPoints + numOfForecastPoints; p++) {
+            for (int p = 0; p < Repository.numOfRealPoints + Repository.numOfForecastPoints; p++) {
                 AxisValue valueX = new AxisValue(p);//这里的数字是坐标的数值，比如第一个坐标就是0
                 //将坐标的数值和对应的文字标签绑定起来
                 //分为前（真实）后（预测）
-                if (p < numOfRealPoints)
+                if (p < Repository.numOfRealPoints)
                     //前，是真实线0的标签
                     valueX.setLabel(axisLableList.get(0).get(p));
                 else
                 //后，是预测线i的标签，此时的点值应该剪掉前面的真实点总数
                 {
-                    int thisLineIndex = i + numOfRealLines;
-                    int thisP = p - numOfRealPoints;
+                    int thisLineIndex = i + Repository.numOfRealLines;
+                    int thisP = p - Repository.numOfRealPoints;
                     valueX.setLabel(axisLableList.get(thisLineIndex).get(thisP));
                 }
                 valueListX.add(valueX);//添加一个值
@@ -149,20 +115,10 @@ public class LineViewModel extends ViewModel {
      */
     public void initRealChart() {
         Log.i(TAG, "initRealChart 进入函数");
-
-        //数
-        for (int i = 0; i < numOfRealLines; ++i) {
-            float[] linePoints = new float[numOfRealPoints];//一条线上面的点
-            for (int j = 0; j < numOfRealPoints; ++j) {
-                Random random = new Random();
-                linePoints[j] = random.nextInt(50) + j * 10;
-            }
-            lineData.add(linePoints);
-        }
         //线
-        for (int i = 0; i < numOfRealLines; i++) {
+        for (int i = 0; i < Repository.numOfRealLines; i++) {
             List<PointValue> tempArrayList = new ArrayList<>();//一条线的数据
-            for (int j = 0; j < numOfRealPoints; j++) {
+            for (int j = 0; j < Repository.numOfRealPoints; j++) {
                 tempArrayList.add(new PointValue(j, lineData.get(i)[j]));
             }
             Line line = new Line(tempArrayList);//根据值来创建一条线
@@ -176,10 +132,10 @@ public class LineViewModel extends ViewModel {
         }
         //轴
         //建立标签
-        for (int i = 0; i < numOfRealLines; i++) {
-            List<String> strings = new ArrayList<>(numOfRealPoints);
+        for (int i = 0; i < Repository.numOfRealLines; i++) {
+            List<String> strings = new ArrayList<>(Repository.numOfRealPoints);
             int day = 0;
-            for (int j = 0; j < numOfRealPoints; j++) {
+            for (int j = 0; j < Repository.numOfRealPoints; j++) {
                 day++;
                 if (day > 30) {
                     day %= 30;
@@ -190,11 +146,11 @@ public class LineViewModel extends ViewModel {
             axisLableList.add(strings);
         }
         //绑定标签和轴
-        for (int i = 0; i < numOfRealLines; i++) {
+        for (int i = 0; i < Repository.numOfRealLines; i++) {
             Axis axisX = new Axis();//新建一个x轴
             List<AxisValue> valueListX = new ArrayList<>();//新建一个x轴的值列表
             //每个点
-            for (int j = 0; j < numOfRealPoints; j++) {
+            for (int j = 0; j < Repository.numOfRealPoints; j++) {
                 AxisValue valueX = new AxisValue(j);//这里的数字是坐标的数值，比如第一个坐标就是0
                 valueX.setLabel(axisLableList.get(i).get(j));//将坐标的数值和对应的文字标签绑定起来
                 valueListX.add(valueX);//添加一个值
@@ -209,7 +165,7 @@ public class LineViewModel extends ViewModel {
 
     //getter & setter
     public int getNumOfRealLines() {
-        return numOfRealLines;
+        return Repository.numOfRealLines;
     }
 
     public List<Line> getLines() {
