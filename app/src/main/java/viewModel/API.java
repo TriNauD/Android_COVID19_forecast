cd3a8b4e1b659304068d85ee1b75f572b5ef8888
lwftdr <44592483+lwftdr@users.noreply.github.com>
2020-06-11 00:11:57 +0800

    得到数据列表

--- app/src/main/java/viewModel/API.java
+++ app/src/main/java/viewModel/API.java
@@ -1,4 +1,21 @@
 package viewModel;
 
+
+import java.util.List;
+
+import domain.Alltime_province;
+import domain.Alltime_world;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface API {
+    @GET("/getProvince")
+    Call<List<Alltime_province>> getProvince(@Query("name") String name);
+
+    @GET("/getWorld")
+    Call<List<Alltime_world>> getWorld(@Query("name") String name);
+
+    @GET("/getPredict")
+    Call<List<Integer>> getPredict(@Query("name") String name);
 }
