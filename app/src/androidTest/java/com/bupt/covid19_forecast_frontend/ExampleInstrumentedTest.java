dc279fb80c5490a84e40425ed0eab78632306691
TriNauD <17801202079@163.com>
2020-09-07 17:14:58 +0800

    ExampleInstrumentedTest.java: Now can check if the toast text is as intended.

--- app/src/androidTest/java/com/bupt/covid19_forecast_frontend/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bupt/covid19_forecast_frontend/ExampleInstrumentedTest.java
@@ -1,7 +1,5 @@
 package com.bupt.covid19_forecast_frontend;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,13 +11,15 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
-import android.support.test.runner.AndroidJUnit4;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -36,15 +36,17 @@ public class ExampleInstrumentedTest {
     @Test
     public void testChangeText_sameActivity() {
 
+        MainActivity activity = activityRule.getActivity();
+
         // Type text and then press the button.
         onView(withId(R.id.control_start_date_day_input))
                 .perform(typeText("1"), closeSoftKeyboard());
         onView(withId(R.id.control_start_date_month_input))
                 .perform(typeText("1"), closeSoftKeyboard());
         onView(withId(R.id.submit_button)).perform(click());
-        // Check that the text was changed.
-//        onView(withId(R.id.control_start_date_month_input))
-//                .check(matches(withText("1")));
+        // Check the toast if its text is failure.
+        onView(withText(R.string.alert_msg_get_data_failure)).inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
 
     }
 
