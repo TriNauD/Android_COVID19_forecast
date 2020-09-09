5b68ad77b5d9b207537303cfaca539aac473e41f
TriNauD <17801202079@163.com>
2020-09-09 11:32:59 +0800

    MainActivity.java: Delete useless import.

--- app/src/main/java/com/bupt/covid19_forecast_frontend/MainActivity.java
+++ app/src/main/java/com/bupt/covid19_forecast_frontend/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
