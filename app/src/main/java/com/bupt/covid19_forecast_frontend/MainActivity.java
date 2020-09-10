e7008391a7bb7f0665c275d399416db094142d27
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-09-10 09:33:46 +0800

    去掉了无用的import

--- app/src/main/java/com/bupt/covid19_forecast_frontend/MainActivity.java
+++ app/src/main/java/com/bupt/covid19_forecast_frontend/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
